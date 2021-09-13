e4c7f6720ebea3e85c1151ac772eeb3a96c3c6b7
soldfield <soldfield@users.noreply.github.com>
2021-09-13 15:17:43 +0200

    fracture network examples added

---
+++ src/geoseer/dfngen_reader.py
@@ -0,0 +1,11 @@
+"""
+Scripts to import output from the DFN generator developed by Michael Welch and Mikael Lüthje at DTU.
+
+
+"""
+
+#TODO: Plot 2D representation of centre lines from fracture network
+
+
+#%%
+
